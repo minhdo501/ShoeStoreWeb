c1de43aeb4ebaa4106ca7fd4eedf4784a843d4ce
Minh Do Quang <minh.doquang@petronas.com>
2022-10-04 10:49:35 +0700

    Remove State field from Register form

--- ShoeStoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ShoeStoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -104,7 +104,7 @@ namespace ShoeStoreWeb.Areas.Identity.Pages.Account
             public string Name { get; set; }
             public string? StreetAddress { get; set; }
             public string? City { get; set; }
-            public string? State { get; set; }
+            //public string? State { get; set; }
             public string? PostalCode { get; set; }
             public string? PhoneNumber { get; set; }
             public string? Role { get; set; }
