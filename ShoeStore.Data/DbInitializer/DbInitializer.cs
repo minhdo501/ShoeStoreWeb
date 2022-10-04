a2fc83dfe4dcf816dfa17d19be24658c77169b74
Minh Do Quang <minh.doquang@petronas.com>
2022-10-04 10:55:38 +0700

    config to create only admin and employee roles

--- ShoeStore.Data/DbInitializer/DbInitializer.cs
+++ ShoeStore.Data/DbInitializer/DbInitializer.cs
@@ -42,8 +42,8 @@ namespace ShoeStore.Data.DbInitializer
             {
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
+                //_roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
+                //_roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
 
                 //if roles are not created, then we will create admin user as well
 
